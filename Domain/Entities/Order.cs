a590a88c59cbb82c5a371a1ab31e75612106d645
Magzumov Saidakram <magzumovsaidakram@gmail.com>
2026-03-03 16:17:35 +0500

    WebHook

--- Domain/Entities/Order.cs
+++ Domain/Entities/Order.cs
@@ -6,6 +6,7 @@ public class Order
 {
     public int Id { get; set; }
     public int UserId { get; set; }
+    public string? Description { get; set; }
     public OrderStatus Status { get; set; } = OrderStatus.Pending;
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
--- TelegramBot/Controllers/WebHookMethod.cs
+++ TelegramBot/Controllers/WebHookMethod.cs
@@ -1,29 +1,80 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Domain.Enums;
+using Infrastructure.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 using Telegram.Bot;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
 
 namespace TelegramBot.Controllers;
 
 [ApiController]
 [Route("api/telegram")]
-public class TelegramController(ITelegramBotClient bot) : ControllerBase
+public class TelegramController(
+    ITelegramBotClient bot,
+    IOrderService1 orderService) : ControllerBase
 {
     [HttpPost("webhook")]
     public async Task<IActionResult> Webhook([FromBody] Update update)
     {
-        if (update?.Message?.Text == null)
+        if (update == null)
             return Ok();
-
-        var chatId = update.Message.Chat.Id;
-        var text = update.Message.Text;
-
-        try
+        
+        if (update.Type == UpdateType.Message && update.Message!.Text != null)
         {
-            await bot.SendMessage(chatId, "You said: " + text);
+            var chatId = (int)update.Message.Chat.Id;
+            var text = update.Message.Text;
+
+        
+            if (text.StartsWith("/order"))
+            {
+                await orderService.Create(chatId);
+
+                await bot.SendMessage(
+                    chatId,
+                    "✅ Order created successfully!"
+                );
+            }
+
+           
+            if (text == "/myorders")
+            {
+                var orders = await orderService.GetUserOrders(chatId);
+
+                var message = "📦 Your Orders:\n\n";
+
+                foreach (var o in orders)
+                {
+                    message += $"ID: {o.Id} | Status: {o.Status}\n";
+                }
+
+                await bot.SendMessage(chatId, message);
+            }
         }
-        catch (Exception ex)
+
+       
+        if (update.Type == UpdateType.CallbackQuery)
         {
-            Console.WriteLine(ex.Message);
+            var callback = update.CallbackQuery!;
+            var data = callback.Data;
+
+            if (data!.StartsWith("Cancelled"))
+            {
+                var id = int.Parse(data.Split("_")[1]);
+
+                await orderService.UpdateStatus(id, OrderStatus.Cancelled);
+
+                await bot.AnswerCallbackQuery(callback.Id);
+            }
+
+            if (data.StartsWith("Processing"))
+            {
+                var id = int.Parse(data.Split("_")[1]);
+
+                await orderService.UpdateStatus(id, OrderStatus.Processing);
+
+                await bot.AnswerCallbackQuery(callback.Id);
+            }
         }
 
         return Ok();
